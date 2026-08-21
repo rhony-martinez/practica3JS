6a93246a2dc35838c0dd5fa8586a1a4edf21ed18
RhonyMartinez <mbrhony4806@gmail.com>
2026-08-20 20:51:08 -0500

    Definición de nuevos campos para médico en entidad y html

--- capaDeAccesoADatos/entitys/Medico.js
+++ capaDeAccesoADatos/entitys/Medico.js
@@ -1,8 +1,12 @@
 class Medico {
-  constructor(id, nombres, apellidos) {
+  constructor(id, nombres, apellidos, especialidad, horarioAtencion, aniosExp, biografia) {
     this.id = id;
     this.nombres = nombres;
     this.apellidos = apellidos;
+    this.especialidad = especialidad;
+    this.horarioAtencion = horarioAtencion;
+    this.aniosExp = aniosExp;
+    this.biografia = biografia;
   }
 }
 
